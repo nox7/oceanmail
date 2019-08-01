382f4a3718e01dd3f8281624738067c6f023f0d3
Garet C. Green <garetgreen1996@gmail.com>
2019-08-01 15:39:29 -0500

    Setup post office to handle received mail

---
+++ PostOffice.php
@@ -0,0 +1,30 @@
+<?php
+	/**
+	* Class PostOffice
+	*
+	* @author Garet C. Green
+	*/
+
+	require_once __DIR__ . "/Debug.php";
+	require_once __DIR__ . "/Envelope.php";
+	require_once __DIR__ . "/EmailUtility.php";
+
+	/**
+	* Handles delivering mail
+	*/
+	class PostOffice{
+
+		public function __construct(){
+
+		}
+
+		/**
+		* Receives a dropped off envelope
+		*
+		* @param Envelope $mail
+		* @return void
+		*/
+		public function onMailDroppedOff(Envelope $mail){
+			Debug::log("Envelope received:\n" . $mail, Debug::DEBUG_LEVEL_MEDIUM);
+		}
+	}
--- server.php
+++ server.php
@@ -1,5 +1,7 @@
 <?php
 	require_once "IncomingServer.php";
+	require_once "PostOffice.php";
 
+	$poBox = new PostOffice();
 	$server = new IncomingServer();
-	$server->startServerLoop();
+	$server->startServerLoop($poBox);
