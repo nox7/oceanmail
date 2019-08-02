095c9691070d8c22a370da0fee66e6270ed90b16
Garet C. Green <garetgreen1996@gmail.com>
2019-08-02 10:46:20 -0500

    Setup successful multipart test

--- client.php
+++ client.php
@@ -1,78 +1,32 @@
 <?php
-	mail("test@localhost.com", "Test Email", "Content", [
+	// // Send basic text
+	// mail("test@localhost.com", "Test Email", "Content", [
+	// 	"From"=>"test@example.com",
+	// 	"Return-Path"=>"garet@footbridgemedia.com",
+	// 	"Content-Type"=>"text/html; charset=UTF-8;",
+	// ]);
+
+	// Send multipart/mixed
+	$boundary = "oceanmailboundary" . time();
+	$inTextBoundary = "--" . $boundary;
+	$endBoundary = $inTextBoundary . "--";
+	$message = "";
+
+	$eol = "\r\n";
+	$message .= $eol . $inTextBoundary . $eol;
+	$message .= "Content-Type: text/plain; charset=utf-8" . $eol;
+	$message .= $eol;
+	$message .= "Plain text message";
+	$message .= $eol;
+	$message .= $eol . $inTextBoundary . $eol;
+	$message .= "Content-Type: text/html; charset=utf-8" . $eol;
+	$message .= $eol;
+	$message .= "<strong>Html text</strong>";
+	$message .= $eol;
+	$message .= $eol . $endBoundary . $eol;
+
+	mail("test@localhost.com", "Test Email", $message, [
 		"From"=>"test@example.com",
 		"Return-Path"=>"garet@footbridgemedia.com",
-		"Content-Type"=>"text/html; charset=UTF-8;",
+		"Content-Type"=>"multipart/mixed; charset=UTF-8; boundary=\"$boundary\"",
 	]);
-
-	function sendmail( $param )
-{
-    $from    = &$param[ 'from' ];
-    $to      = &$param[ 'to' ];
-    $message = &$param[ 'data' ];
-
-    $isError = function( $string )
-    {
-        if( preg_match( '/^((\d)(\d{2}))/', $string, $matches ) )
-        {
-            if( $matches[ 2 ] == 4 || $matches[ 2 ] == 5 ) return( $matches[ 1 ] );
-        }
-        else
-        {
-            return( false );
-        }
-    };
-
-    try
-    {
-        $socket = null;
-        if( ( $socket = socket_create( AF_INET, SOCK_STREAM, SOL_TCP ) ) == false )
-        {
-            throw new Exception( sprintf( "Unable to create a socket: %s", socket_strerror( socket_last_error() ) ) );
-        }
-        if( !socket_connect( $socket, MAIL_SERVER, MAIL_PORT ) )
-        {
-            throw new Exception( sprintf( "Unable to connect to server %s: %s", MAIL_SERVER, socket_strerror( socket_last_error() ) ) );
-        }
-        $read = socket_read( $socket, 1024 );
-        if( $read == false )
-        {
-            throw new Exception( sprintf( "Unable to read from socket: %s", socket_strerror( socket_last_error() ) ) );
-        }
-
-        if( socket_write( $socket, sprintf( "HELO %s\r\n", gethostname() ) ) === false )
-        {
-            throw new Exception( sprintf( "Unable to write to socket: %s", socket_strerror( socket_last_error() ) ) );
-        }
-        $read = socket_read( $socket, 1024 );
-        if( $read == false )
-        {
-            throw new Exception( sprintf( "Unable to read from socket: %s", socket_strerror( socket_last_error() ) ) );
-        }
-        else
-        {
-            if( ( $errCode = $isError( $read ) ) ) throw new Exception( "Server responded with an error code $errCode" );
-        }
-
-        if( socket_write( $socket, sprintf( "MAIL FROM: %s\r\n", $from ) ) === false )
-        {
-            throw new Exception( sprintf( "Unable to write to socket: %s", socket_strerror( socket_last_error() ) ) );
-        }
-        $read = socket_read( $socket, 1024 );
-        if( $read == false )
-        {
-            throw new Exception( sprintf( "Unable to read from socket: %s", socket_strerror( socket_last_error() ) ) );
-        }
-        else
-        {
-            if( ( $errCode = $isError( $read ) ) ) throw new Exception( "Server responded with an error code $errCode" );
-        }
-        /* And some more code, but not enough place in comment */
-        return( $totalWriten );
-    }
-    catch( Exception $e )
-    {
-        $ERROR = sprintf( "Error sending mail message at line %d. ", $e->getLine() ) . $e->getMessage();
-        return( false );
-    }
-}
