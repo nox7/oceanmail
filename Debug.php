29d85ea0c7be17e264c93b943356d57d958bc52d
Garet C. Green <garetgreen1996@gmail.com>
2019-08-05 10:43:31 -0500

    Properly document Debug class contents

--- Debug.php
+++ Debug.php
@@ -1,5 +1,6 @@
 <?php
-	/** Debug Class
+	/**
+	* Debug Class
 	*
 	* @author Garet C. Green
 	*/
@@ -9,13 +10,28 @@
 	*/
 	class Debug{
 
-		const DEBUG_LEVEL_LOW = 0; // Output low and up
-		const DEBUG_LEVEL_MEDIUM = 1; // Output medium and up
-		const DEBUG_LEVEL_HIGH = 2; // Output high and up
+		/** @var int Constant for low-priority logs */
+		const DEBUG_LEVEL_LOW = 0;
 
+		/** @var int Constant for medium-priority logs */
+		const DEBUG_LEVEL_MEDIUM = 1;
+
+		/** @var int Constant for high-priority logs */
+		const DEBUG_LEVEL_HIGH = 2;
+
+		/** @var int Current debug level */
 		const DEBUG_LEVEL = 0;
+
+		/** @var bool Whether or not debugging is on at all */
 		const DEBUG_ON = true;
 
+		/**
+		* Outputs a string if the current debug level is low enough to allow it
+		*
+		* @param string $message
+		* @param int $debugLevel
+		* @return void
+		*/
 		public static function log(string $message, int $debugLevel){
 
 			if ($debugLevel >= self::DEBUG_LEVEL){
